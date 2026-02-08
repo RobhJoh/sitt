a88960959fcc47574703e80da33a8d3ab467bba8
Roberth Johansson <roberth.johansson@cdab.eu>
2026-02-08 12:21:25 +0100

    fix: Update version in package.json to 1.2.3 and change edition in roles.json to 'exp' and improved majority count

--- src/components/Vote.vue
+++ src/components/Vote.vue
@@ -6,50 +6,31 @@
     </div>
     <div class="overlay">
       <audio src="../assets/sounds/countdown.mp3" preload="auto"></audio>
-      <em class="blue">{{ nominator.name }}</em> nominerade
-      <em>{{ nominee.name }}</em
+      <em class="blue">{{ nominator.name }}</em> nominerade <em>{{ nominee.name }}</em
       >!
       <br />
-      <em class="blue">
-        {{ voters.length }} röst{{ voters.length !== 1 ? "er" : "" }}
-      </em>
+      <em class="blue"> {{ voters.length }} röst{{ voters.length !== 1 ? 'er' : '' }} </em>
       för
-      <em v-if="nominee.role.team !== 'traveler'">
-        (majoritet är {{ Math.ceil(alive / 2) }})
-      </em>
-      <em v-else>(majoritet är {{ Math.ceil(players.length / 2) }})</em>
+      <em v-if="nominee.role.team !== 'traveler'"> (majoritet är {{ Math.floor(alive / 2) + 1 }}) </em>
+      <em v-else>(majoritet är {{ Math.floor(players.length / 2) + 1 }})</em>
 
       <template v-if="!session.isSpectator">
         <div v-if="!session.isVoteInProgress && session.lockedVote < 1">
           Tid per spelare:
-          <font-awesome-icon
-            @mousedown.prevent="setVotingSpeed(-500)"
-            icon="minus-circle"
-          />
+          <font-awesome-icon @mousedown.prevent="setVotingSpeed(-500)" icon="minus-circle" />
           {{ session.votingSpeed / 1000 }}s
-          <font-awesome-icon
-            @mousedown.prevent="setVotingSpeed(500)"
-            icon="plus-circle"
-          />
+          <font-awesome-icon @mousedown.prevent="setVotingSpeed(500)" icon="plus-circle" />
         </div>
         <div class="button-group">
-          <div
-            class="button townsfolk"
-            v-if="!session.isVoteInProgress"
-            @click="countdown"
-          >
+          <div class="button townsfolk" v-if="!session.isVoteInProgress" @click="countdown">
             Nedräkning
           </div>
           <div class="button" v-if="!session.isVoteInProgress" @click="start">
-            {{ session.lockedVote ? "Återställ" : "Starta" }}
+            {{ session.lockedVote ? 'Återställ' : 'Starta' }}
           </div>
           <template v-else>
-            <div
-              class="button townsfolk"
-              :class="{ disabled: !session.lockedVote }"
-              @click="pause"
-            >
-              {{ voteTimer ? "Paus" : "Återstart" }}
+            <div class="button townsfolk" :class="{ disabled: !session.lockedVote }" @click="pause">
+              {{ voteTimer ? 'Paus' : 'Återstart' }}
             </div>
             <div class="button" @click="stop">Återställ</div>
           </template>
@@ -59,7 +40,7 @@
           <div
             class="button"
             :class="{
-              disabled: session.nomination[1] === session.markedPlayer
+              disabled: session.nomination[1] === session.markedPlayer,
             }"
             @click="setMarked"
           >
@@ -71,22 +52,12 @@
         </div>
       </template>
       <template v-else-if="canVote">
-        <div v-if="!session.isVoteInProgress">
-          {{ session.votingSpeed / 1000 }} sekunder mellan röster
-        </div>
+        <div v-if="!session.isVoteInProgress">{{ session.votingSpeed / 1000 }} sekunder mellan röster</div>
         <div class="button-group">
-          <div
-            class="button townsfolk"
-            @click="vote(false)"
-            :class="{ disabled: !currentVote }"
-          >
+          <div class="button townsfolk" @click="vote(false)" :class="{ disabled: !currentVote }">
             Hand NER
           </div>
-          <div
-            class="button demon"
-            @click="vote(true)"
-            :class="{ disabled: currentVote }"
-          >
+          <div class="button demon" @click="vote(true)" :class="{ disabled: currentVote }">
             Hand UPP
           </div>
         </div>
@@ -96,166 +67,149 @@
       </div>
     </div>
     <transition name="blur">
-      <div
-        class="countdown"
-        v-if="session.isVoteInProgress && !session.lockedVote"
-      >
+      <div class="countdown" v-if="session.isVoteInProgress && !session.lockedVote">
         <span>3</span>
         <span>2</span>
         <span>1</span>
         <span>BÖRJA</span>
-        <audio
-          :autoplay="!grimoire.isMuted"
-          src="../assets/sounds/countdown.mp3"
-          :muted="grimoire.isMuted"
-        ></audio>
+        <audio :autoplay="!grimoire.isMuted" src="../assets/sounds/countdown.mp3" :muted="grimoire.isMuted"></audio>
       </div>
     </transition>
   </div>
 </template>
 
 <script>
-import { mapGetters, mapState } from "vuex";
+import { mapGetters, mapState } from 'vuex'
 
 export default {
   computed: {
-    ...mapState("players", ["players"]),
-    ...mapState(["session", "grimoire"]),
-    ...mapGetters({ alive: "players/alive" }),
+    ...mapState('players', ['players']),
+    ...mapState(['session', 'grimoire']),
+    ...mapGetters({ alive: 'players/alive' }),
     nominator: function() {
-      return this.players[this.session.nomination[0]];
+      return this.players[this.session.nomination[0]]
     },
     nominatorStyle: function() {
-      const players = this.players.length;
-      const nomination = this.session.nomination[0];
+      const players = this.players.length
+      const nomination = this.session.nomination[0]
       return {
         transform: `rotate(${Math.round((nomination / players) * 360)}deg)`,
-        transitionDuration: this.session.votingSpeed - 100 + "ms"
-      };
+        transitionDuration: this.session.votingSpeed - 100 + 'ms',
+      }
     },
     nominee: function() {
-      return this.players[this.session.nomination[1]];
+      return this.players[this.session.nomination[1]]
     },
     nomineeStyle: function() {
-      const players = this.players.length;
-      const nomination = this.session.nomination[1];
-      const lock = this.session.lockedVote;
-      const rotation = (360 * (nomination + Math.min(lock, players))) / players;
+      const players = this.players.length
+      const nomination = this.session.nomination[1]
+      const lock = this.session.lockedVote
+      const rotation = (360 * (nomination + Math.min(lock, players))) / players
       return {
         transform: `rotate(${Math.round(rotation)}deg)`,
-        transitionDuration: this.session.votingSpeed - 100 + "ms"
-      };
+        transitionDuration: this.session.votingSpeed - 100 + 'ms',
+      }
     },
     player: function() {
-      return this.players.find(p => p.id === this.session.playerId);
+      return this.players.find((p) => p.id === this.session.playerId)
     },
     currentVote: function() {
-      const index = this.players.findIndex(p => p.id === this.session.playerId);
-      return index >= 0 ? !!this.session.votes[index] : undefined;
+      const index = this.players.findIndex((p) => p.id === this.session.playerId)
+      return index >= 0 ? !!this.session.votes[index] : undefined
     },
     canVote: function() {
-      if (!this.player) return false;
-      if (this.player.isVoteless && this.nominee.role.team !== "traveler")
-        return false;
-      const session = this.session;
-      const players = this.players.length;
-      const index = this.players.indexOf(this.player);
-      const indexAdjusted =
-        (index - 1 + players - session.nomination[1]) % players;
-      return indexAdjusted >= session.lockedVote - 1;
+      if (!this.player) return false
+      if (this.player.isVoteless && this.nominee.role.team !== 'traveler') return false
+      const session = this.session
+      const players = this.players.length
+      const index = this.players.indexOf(this.player)
+      const indexAdjusted = (index - 1 + players - session.nomination[1]) % players
+      return indexAdjusted >= session.lockedVote - 1
     },
     voters: function() {
-      const nomination = this.session.nomination[1];
+      const nomination = this.session.nomination[1]
       const voters = Array(this.players.length)
-        .fill("")
-        .map((x, index) =>
-          this.session.votes[index] ? this.players[index].name : ""
-        );
-      const reorder = [
-        ...voters.slice(nomination + 1),
-        ...voters.slice(0, nomination + 1)
-      ];
-      return (this.session.lockedVote
-        ? reorder.slice(0, this.session.lockedVote - 1)
-        : reorder
-      ).filter(n => !!n);
-    }
+        .fill('')
+        .map((x, index) => (this.session.votes[index] ? this.players[index].name : ''))
+      const reorder = [...voters.slice(nomination + 1), ...voters.slice(0, nomination + 1)]
+      return (this.session.lockedVote ? reorder.slice(0, this.session.lockedVote - 1) : reorder).filter((n) => !!n)
+    },
   },
   data() {
     return {
-      voteTimer: null
-    };
+      voteTimer: null,
+    }
   },
   methods: {
     countdown() {
-      this.$store.commit("session/lockVote", 0);
-      this.$store.commit("session/setVoteInProgress", true);
+      this.$store.commit('session/lockVote', 0)
+      this.$store.commit('session/setVoteInProgress', true)
       this.voteTimer = setInterval(() => {
-        this.start();
-      }, 4000);
+        this.start()
+      }, 4000)
     },
     start() {
-      this.$store.commit("session/lockVote", 1);
-      this.$store.commit("session/setVoteInProgress", true);
-      clearInterval(this.voteTimer);
+      this.$store.commit('session/lockVote', 1)
+      this.$store.commit('session/setVoteInProgress', true)
+      clearInterval(this.voteTimer)
       this.voteTimer = setInterval(() => {
-        this.$store.commit("session/lockVote");
+        this.$store.commit('session/lockVote')
         if (this.session.lockedVote > this.players.length) {
-          clearInterval(this.voteTimer);
-          this.$store.commit("session/setVoteInProgress", false);
+          clearInterval(this.voteTimer)
+          this.$store.commit('session/setVoteInProgress', false)
         }
-      }, this.session.votingSpeed);
+      }, this.session.votingSpeed)
     },
     pause() {
       if (this.voteTimer) {
-        clearInterval(this.voteTimer);
-        this.voteTimer = null;
+        clearInterval(this.voteTimer)
+        this.voteTimer = null
       } else {
         this.voteTimer = setInterval(() => {
-          this.$store.commit("session/lockVote");
+          this.$store.commit('session/lockVote')
           if (this.session.lockedVote > this.players.length) {
-            clearInterval(this.voteTimer);
-            this.$store.commit("session/setVoteInProgress", false);
+            clearInterval(this.voteTimer)
+            this.$store.commit('session/setVoteInProgress', false)
           }
-        }, this.session.votingSpeed);
+        }, this.session.votingSpeed)
       }
     },
     stop() {
-      clearInterval(this.voteTimer);
-      this.voteTimer = null;
-      this.$store.commit("session/setVoteInProgress", false);
-      this.$store.commit("session/lockVote", 0);
+      clearInterval(this.voteTimer)
+      this.voteTimer = null
+      this.$store.commit('session/setVoteInProgress', false)
+      this.$store.commit('session/lockVote', 0)
     },
     finish() {
-      clearInterval(this.voteTimer);
-      this.$store.commit("session/addHistory", this.players);
-      this.$store.commit("session/nomination");
+      clearInterval(this.voteTimer)
+      this.$store.commit('session/addHistory', this.players)
+      this.$store.commit('session/nomination')
     },
     vote(vote) {
-      if (!this.canVote) return false;
-      const index = this.players.findIndex(p => p.id === this.session.playerId);
+      if (!this.canVote) return false
+      const index = this.players.findIndex((p) => p.id === this.session.playerId)
       if (index >= 0 && !!this.session.votes[index] !== vote) {
-        this.$store.commit("session/voteSync", [index, vote]);
+        this.$store.commit('session/voteSync', [index, vote])
       }
     },
     setVotingSpeed(diff) {
-      const speed = Math.round(this.session.votingSpeed + diff);
+      const speed = Math.round(this.session.votingSpeed + diff)
       if (speed > 0) {
-        this.$store.commit("session/setVotingSpeed", speed);
+        this.$store.commit('session/setVotingSpeed', speed)
       }
     },
     setMarked() {
-      this.$store.commit("session/setMarkedPlayer", this.session.nomination[1]);
+      this.$store.commit('session/setMarkedPlayer', this.session.nomination[1])
     },
     removeMarked() {
-      this.$store.commit("session/setMarkedPlayer", -1);
-    }
-  }
-};
+      this.$store.commit('session/setMarkedPlayer', -1)
+    },
+  },
+}
 </script>
 
 <style lang="scss" scoped>
-@import "../vars.scss";
+@import '../vars.scss';
 
 #vote {
   position: absolute;
@@ -265,11 +219,10 @@ export default {
   align-items: center;
   align-content: center;
   justify-content: center;
-  background: url("../assets/sitt-logo.svg") center center no-repeat;
+  background: url('../assets/sitt-logo.svg') center center no-repeat;
   background-size: auto 75%;
   text-align: center;
-  text-shadow: 0 1px 2px #000000, 0 -1px 2px #000000, 1px 0 2px #000000,
-    -1px 0 2px #000000;
+  text-shadow: 0 1px 2px #000000, 0 -1px 2px #000000, 1px 0 2px #000000, -1px 0 2px #000000;
 
   .overlay {
     background: black;
@@ -284,7 +237,7 @@ export default {
   }
 
   &:after {
-    content: " ";
+    content: ' ';
     padding-bottom: 100%;
     display: block;
   }
@@ -343,7 +296,7 @@ export default {
     transition: transform 2.9s ease-in-out;
   }
   span:before {
-    content: " ";
+    content: ' ';
     width: 100%;
     height: 100%;
     display: block;
@@ -354,11 +307,11 @@ export default {
     filter: drop-shadow(0px 0px 3px #000);
   }
   .nominator:before {
-    background-image: url("../assets/clock-small.png");
+    background-image: url('../assets/clock-small.png');
     animation: arrow-ccw 1s ease-out;
   }
   .nominee:before {
-    background-image: url("../assets/clock-big.png");
+    background-image: url('../assets/clock-big.png');
     animation: arrow-cw 1s ease-out;
   }
 }
